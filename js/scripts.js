a34200440c0af42483ddb7f32c7052b58ef779e2
Anna Lawley and Michelle Brecunier <alawley1@gmail.com>
2016-01-19 09:58:31 -0800

    add validating function and user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
 var leapYear = function(year) {
-  if ((year <= 0) || isNaN(year)) {
-    return false;
-  }
-  else if (year < 1){
-    return false;
-  } else if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
   }
 };
+
+var validateEntry = function(year) {
+  if ((year <= 0) || isNaN(year)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    validateEntry();
+
+    if(validateEntry) {
+      if (!result) {
+        $(".not").text("not");
+      } else {
+        $(".not").text("");
+      }
+      $(".year").text(year);
+
+      $("#result").show();
+    } else {
+      $("#error").show();
+      return false;
+    }
+
+    event.preventDefault();
+  });
+});
