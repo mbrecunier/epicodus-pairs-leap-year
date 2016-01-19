81c262667c2c4e6cef7adecde751685d9967407b
Anna Lawley and Michelle Brecunier <alawley1@gmail.com>
2016-01-19 08:50:50 -0800

    remove refactor to make further additions easier

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 var leapYear = function(year) {
-  return ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0))
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
